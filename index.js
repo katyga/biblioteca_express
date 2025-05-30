4a500cfdeb14e3cf2fad94b340577b32cc9048a5
kathllyn <ksoaresmartins202@gmail.com>
2025-05-30 09:23:36 -0300

    1

--- index.js
+++ index.js
@@ -99,6 +99,87 @@ app.delete('/categorias/:id', async (req, res) => {
   }
 });
 
+// Listar todas as aluno
+app.get('/aluno', async (req, res) => {
+  try {
+    const result = await pool.query('SELECT * FROM aluno');
+    res.json(result.rows);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao buscar aluno');
+  }
+});
+
+// Buscar aluno por ID
+app.get('/aluno/:id', async (req, res) => {
+  const { id } = req.params;
+
+  try {
+    const result = await pool.query('SELECT * FROM aluno WHERE id = $1', [id]);
+
+    if (result.rows.length === 0) {
+      return res.status(404).send('aluno não encontrada');
+    }
+
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao buscar aluno por ID');
+  }
+});
+
+// Criar uma nova aluno
+app.post('/aluno', async (req, res) => {
+  const { nome } = req.body;
+  try {
+    const result = await pool.query(
+      'INSERT INTO categoria (nome) VALUES ($1) RETURNING *',
+      [nome]
+    );
+    res.status(201).json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao criar aluno');
+  }
+});
+
+// Atualizar uma aluno pelo id
+app.put('/aluno/:id', async (req, res) => {
+  const id = req.params.id;
+  const { nome } = req.body;
+  try {
+    const result = await pool.query(
+      'UPDATE aluno SET nome = $1 WHERE id = $2 RETURNING *',
+      [nome, id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).send('aluno não encontrada');
+    }
+    res.json(result.rows[0]);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao atualizar aluno');
+  }
+});
+
+// Deletar uma aluno pelo id
+app.delete('/aluno/:id', async (req, res) => {
+  const id = req.params.id;
+  try {
+    const result = await pool.query(
+      'DELETE FROM aluno WHERE id = $1 RETURNING *',
+      [id]
+    );
+    if (result.rows.length === 0) {
+      return res.status(404).send('Categoria não encontrada');
+    }
+    res.send('Categoria deletada com sucesso');
+  } catch (err) {
+    console.error(err);
+    res.status(500).send('Erro ao deletar aluno');
+  }
+});
+
 
 /*
 
